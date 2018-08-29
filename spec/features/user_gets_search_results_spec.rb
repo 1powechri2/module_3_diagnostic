06e644eb1abca007255b94ff4e60c458ac4a8b15
Chris Powell <chris.ian.powell@gmail.com>
2018-08-29 09:26:18 -0600

    adds half of feature test for user story

---
+++ spec/features/user_gets_search_results_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203 (Note: Use the existing search form)
+# And I click "Locate"
+# Then I should be on page "/search"
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+
+describe 'A user searches for fuel stations' do
+  context 'from the root page' do
+    it 'entering their zipcode to see stations nearby' do
+      visit '/'
+
+      within(".navbar") do
+        fill_in :q, with: 80203
+        click_on "Locate"
+      end
+
+      expect(current_path).to eq.(search_path)
+    end
+  end
+end
